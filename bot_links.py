360c8ae63631a838c52a3898fe4a06949b0ceae3
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-12-28 22:26:04 -0300

    links separados

---
+++ bot_links.py
@@ -0,0 +1,5 @@
+banido = 'https://tenor.com/view/ednaldo-pereira-banido-desbanido-meme-meme-banido-gif-19429642'
+desbanido = 'https://tenor.com/view/desbanido-ednaldo-pereira-ednaldo-pereira-banido-gif-19443137'
+repositorio = 'https://github.com/Eduardo-Barreto/Marselo-Bot'
+readme = 'https://github.com/Eduardo-Barreto/Marselo-Bot/blob/master/README.md'
+rickrolling = 'https://youtu.be/dQw4w9WgXcQ'
