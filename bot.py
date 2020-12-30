f71415715fd0990f63a07ad3cac014592c4af17a
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-12-30 20:21:14 -0300

    latencia no segundo ping corrigida

--- bot.py
+++ bot.py
@@ -194,7 +194,7 @@ async def ping(ctx):
     else:
         await ctx.send(f'demorei {ping}ms, o mesmo que da ultima vez!')
     last_ping = ping
-    await ctx.send(f'Mas o segundo ping é de {round(bot.latency * 100)}ms')
+    await ctx.send(f'Mas o segundo ping é de {round(bot.latency * 1000)}ms')
     return
 
 
