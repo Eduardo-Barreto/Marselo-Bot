995f8d3c09b12112c365bacd31e9d39f3b7aabd7
Eduardo-Barreto <eduwagzozo@gmail.com>
2021-03-30 16:14:41 -0300

    tokens registrados com Config Vars

---
+++ tokens.py
@@ -0,0 +1,7 @@
+import os
+
+discord = os.environ['discord']
+eduardo_id = os.environ['eduardo_id']
+conexao_username = os.environ['conexao_username']
+conexao_password = os.environ['conexao_password']
+conexao_token = os.environ['conexao_token']
